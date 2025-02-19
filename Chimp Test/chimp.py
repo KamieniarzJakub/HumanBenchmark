af8fbd850dc2ec439c9c5c38dcc8396c653c2ac6
KamieniarzJakub <jakub.kamieniarz@student.put.poznan.pl>
2025-02-19 13:36:06 +0100

    Slow, but working

--- Chimp Test/chimp.py
+++ Chimp Test/chimp.py
@@ -2,77 +2,68 @@ import pyautogui
 import numpy as np
 import time
 import webbrowser
-import imgkit
+import easyocr
+import cv2
 
 URL = "https://humanbenchmark.com/tests/chimp"
 
 MAX_LEVEL = 41
+
+BLUE = np.array([43, 135, 209])
+NUMBER_BORDER_COLOR = np.array([68, 152, 212])
+
 
 def open_human_benchmark():
     webbrowser.open(URL, 1, True)
     time.sleep(1)
 
 def start_game():
-    #generate_numbers()
+    
     button_location = pyautogui.locateCenterOnScreen("StartButton.png", confidence=0.9)
     for level in range(3, MAX_LEVEL):
-        time.sleep(1)
+        time.sleep(0.5)
         pyautogui.click(button_location)
         pyautogui.screenshot(f"level {level}.png")
-        
-        for x in remember_board():
-            time.sleep(0.5)
-            pyautogui.click(x)
-        
-def generate_numbers():
-    html_code = """
-        <html>
-        <head>
-            <style>
-            body {
-            margin: 0;
-            }
-            div {
-                width: 80px;
-                height: 80px;
-                background-color: #3498db;
-                color: white;
-                font-size: 65px;
-                text-align: center;
-                line-height: 80px;
-                font-family: Helvetica, Arial, sans-serif;
-                padding: 0;
-                font-weight: 400;
-                outline: 0;
-                margin: 0;
-                box-sizing: border-box;
-            }
-            </style>
-        </head>
-        <body>
-            <div>1</div>
-        </body>
-        </html>
-        """
-    for i in range(1, MAX_LEVEL):
-        f = open("number.html", "w")
-        f.write(html_code.replace("<div>1</div>", f"<div>{i}</div>"))
-        f.close()
-        options = {
-            "width": 80,
-            "height": 80,
-        }
-        config = imgkit.config(wkhtmltoimage=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe")
-        imgkit.from_file(filename="number.html", output_path=f"{i}.png", config=config, options=options)
 
+        positions = sorted(remember_board(), key=lambda x: x[0])
         
+        for x in positions:
+            pyautogui.click(x[1])
+
+def determine_game_size():
+    screenWidth, screenHeight = pyautogui.size()
+    game_lower_border = -1
+    game_upper_border = screenHeight
+    screenshot = np.array(pyautogui.screenshot())
+    for y in range(screenHeight):
+        if np.array_equal(screenshot[y][screenWidth//2], BLUE):
+            if game_lower_border < 0:
+                game_lower_border = y
+            else:
+                game_upper_border = y
+    return (screenWidth//10, int(game_lower_border), int(0.8*screenWidth), int(game_upper_border-game_lower_border)), screenWidth//10, game_lower_border
+
 def remember_board():
     positions = []
-    for i in range(1, MAX_LEVEL):
-        try:
-            positions.append(pyautogui.locateCenterOnScreen(f"{i}.png", confidence=0.9))
-        except:
-            break
+    game_size, x_border, y_border = determine_game_size()
+    screenshot = np.array(pyautogui.screenshot("level.png", region=game_size))
+    for y in range(len(screenshot)-100):
+        for x in range(len(screenshot[y])-100):
+            if np.array_equal(screenshot[y][x], BLUE):
+                continue
+            image = screenshot[y:y+100, x:x+100]
+            cv2.imwrite(f"number.png", image)
+            image = cv2.imread(f"number.png", cv2.IMREAD_GRAYSCALE)
+            _, image = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
+            reader = easyocr.Reader(["en"])
+            data = reader.readtext(image, allowlist="1234567890", detail=0)
+            screenshot[y-10:y+100, x-10:x+100] = BLUE
+            print(data)
+            try:
+                positions.append([int(data[0]), (x+x_border+50, y+y_border+50)])
+            except:
+                positions.append([7, (x+x_border+50, y+y_border+50)])
+    print()
     return positions
 
 open_human_benchmark()
